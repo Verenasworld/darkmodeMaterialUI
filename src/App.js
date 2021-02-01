4cb3071355c09bc46a79ff9f0701c1544f0c76e3
Verenasworld <verena.felizidas@gmail.com>
2021-02-01 16:59:33 +0100

    create dark mode with Material UI tutorial use theme

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
-import logo from './logo.svg';
+
 import './App.css';
+import {Switch, Grid, Typography, Button, Paper} from "@material-ui/core";
+import {ThemeProvider, createMuiTheme} from "@material-ui/core/styles";
+import {useState} from "react";
+
+
+
 
 function App() {
+
+    const [darkMode, setDarkMode] = useState(false)
+    const theme = createMuiTheme({
+        palette: {
+            type: darkMode ? "dark" : "light",
+        }
+    })
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+        <Paper style={{ height: "100vh"}}>
+            <Switch checked={darkMode} onChange={() => setDarkMode(!darkMode)}/>
+            <Grid container direction="column">
+                <Typography variant="h1"> This is great</Typography>
+                <Button variant="contained" color="primary">
+                    this is a button
+                </Button>
+                <Button variant="contained" color="secondary">
+                    this is also a great button
+                </Button>
+            </Grid>
+        </Paper>
+    </ThemeProvider>
   );
 }
 
